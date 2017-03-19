c9be687f1eb2e86edae732a4aaf025e5d01889f4
michu1234 <moooderator@gmail.com>
2017-03-19 10:19:16 +0100

    Responsywna wersja 2.0

--- js/jQuery.js
+++ js/jQuery.js
@@ -6,14 +6,11 @@ $(document).ready(function(){
 		$(".container").fadeOut("fast", "swing");
 	});
 
-
-
 	// OPEN SUBPAGE
 
 	$(".blockcolor").click(function(){
 		$(".wrapper").addClass("lightSpeedIn");
 	});
-
 
 	$(".blockcolor3").click(function(){
 		$(".wrapper2").addClass("lightSpeedIn");
@@ -28,37 +25,29 @@ $(document).ready(function(){
 	$(".blockcolor54").click(function(){
 		$(".wrapper5").addClass("lightSpeedIn");
 	});
-
-
-
+	$(".blockcolor66").click(function(){
+		$(".wrapper6").addClass("lightSpeedIn");
+	});
+	$(".blockcolor5").click(function(){
+		$(".wrapper6").addClass("lightSpeedIn");
+	});
 
 	// BACK TO MAIN WINDOW
 
-
 	$(".ion-bug2").click(function(){
-		$(".wrapper").removeClass("lightSpeedIn");
-	});
-
-	$(".ion-bug2").click(function(){
-		$(".wrapper2").removeClass("lightSpeedIn");
-	});
-	$(".ion-bug2").click(function(){
-		$(".wrapper3").removeClass("lightSpeedIn");
-	});
-	$(".ion-bug2").click(function(){
-		$(".wrapper4").removeClass("lightSpeedIn");
-	});
-	$(".ion-bug2").click(function(){
-		$(".wrapper5").removeClass("lightSpeedIn");
+		$(".wrapper, .wrapper2, .wrapper3, .wrapper4, .wrapper5, .wrapper6").removeClass("lightSpeedIn");
 	});
 
 	$(".ion-bug2").click(function(){
 		$(".container").fadeIn("slow");
 	});
 
+	// RANDOM SLIDE IN
 
-
-
-
+	$(".ion-arrow-left-c").click(function(){
+	var len = $(".slide").length;
+		random = Math.floor(Math.random() * 6);
+		$("div.slide").removeClass("lightSpeedIn").eq(random).addClass("lightSpeedIn");
+	});
 
 });
