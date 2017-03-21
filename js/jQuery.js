e5025bdc1fe22eb394e70463382ef100687b0002
michu1234 <moooderator@gmail.com>
2017-03-21 06:49:04 +0100

    Testowa

--- js/jQuery.js
+++ js/jQuery.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	// OFF MAIN WINDOW
 
 	$(".off").click(function(){
-		$(".container").fadeOut("fast", "swing");
+		$(".container2").fadeOut("fast", "swing");
 	});
 
 	// OPEN SUBPAGE
@@ -25,27 +25,27 @@ $(document).ready(function(){
 	$(".blockcolor54").click(function(){
 		$(".wrapper5").addClass("lightSpeedIn");
 	});
-	$(".blockcolor66").click(function(){
-		$(".wrapper6").addClass("lightSpeedIn");
-	});
 	$(".blockcolor5").click(function(){
 		$(".wrapper6").addClass("lightSpeedIn");
 	});
+   $(".blockcolor66").click(function(){
+      $(".wrapper7").removeClass("off3").addClass("lightSpeedIn");
+   });
 
 	// BACK TO MAIN WINDOW
 
 	$(".ion-bug2").click(function(){
-		$(".wrapper, .wrapper2, .wrapper3, .wrapper4, .wrapper5, .wrapper6").removeClass("lightSpeedIn");
+     $(".wrapper, .wrapper2, .wrapper3, .wrapper4, .wrapper5, .wrapper6, .wrapper7").removeClass("lightSpeedIn");
 	});
 
 	$(".ion-bug2").click(function(){
-		$(".container").fadeIn("slow");
+		$(".container2").fadeIn("slow");
 	});
 
 	// RANDOM SLIDE IN
 
 	$(".ion-arrow-left-c").click(function(){
-		random = Math.floor(Math.random() * 6);
+		random = Math.floor(Math.random() * 7);
 		$("div.slide").removeClass("lightSpeedIn").eq(random).addClass("lightSpeedIn");
 	});
 
